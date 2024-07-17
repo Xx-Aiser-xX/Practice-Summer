9b644555a14d6b138ccfc70e7b88a8657dccdd3b
Xx-Aiser-xX <stas.plot@mail.ru>
2024-07-17 06:06:29 +0300

    add config

---
+++ src/main/java/org/example/practice/Config/ModelMapperConfig.java
@@ -0,0 +1,14 @@
+package org.example.practice.Config;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ModelMapperConfig {
+
+    @Bean
+    public ModelMapper modelMapper() {
+        return new ModelMapper();
+    }
+}
