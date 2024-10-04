769473d5c7b7c98104c2d8b48b570f6d6c5a77d6
Xx-Aiser-xX <stas.plot@mail.ru>
2024-10-04 03:24:13 +0300

    fix

--- src/main/java/org/example/practice/models/BaseEntity.java
+++ src/main/java/org/example/practice/models/BaseEntity.java
@@ -1,16 +1,14 @@
 package org.example.practice.models;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 
+@MappedSuperclass
 public abstract class BaseEntity {
     protected int id;
 
-    @Id
-    @Column(insertable = false, name = "id")
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+//    @Id
+//    @Column(insertable = false, name = "id")
+//    @GeneratedValue(strategy = GenerationType.IDENTITY)
     public abstract int getId();
     public abstract void setId(int id);
 }
